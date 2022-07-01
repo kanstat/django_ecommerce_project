8dfbf29b02eabb4ba8921be56a392b72bdcd4870
kanstat <kanchan.sharma9722@gmail.com>
2022-07-01 11:49:19 +0530

    updated

--- store/models.py
+++ store/models.py
@@ -24,6 +24,18 @@ class Order(models.Model):
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
@@ -33,6 +45,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    @property
+    def imageUrl(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 
 class ShippingAddress(models.Model):
@@ -50,3 +70,8 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price*self.quantity
+        return total
